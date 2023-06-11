20bb1663c5983d92195889052e55f94dd6e661ee
Shane <77701559+Shane-Bath@users.noreply.github.com>
2023-06-11 15:14:34 +0000

    add models and migrated

--- juneblog/settings.py
+++ juneblog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '(SECRET_KEY)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sb-june-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sb-june-blog.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
