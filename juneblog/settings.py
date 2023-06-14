1a53eb23c5a2fdaf557bbd289ebbc1fef252c87a
Shane <77701559+Shane-Bath@users.noreply.github.com>
2023-06-14 22:12:27 +0000

    add email

--- juneblog/settings.py
+++ juneblog/settings.py
@@ -143,3 +143,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
